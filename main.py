66abbac0e52847f32859607d8e79d713c61319d3
VictorF313 <victorf7833@gmail.com>
2026-02-12 22:27:07 +0530

    Removing image from About tab

--- main.py
+++ main.py
@@ -354,7 +354,4 @@ with tab2.container():
         tab5.dataframe(tableData)
 
 with tab3.container():
-    col3, col4 = st.columns([1, 5])
-    col3.image("https://avatars.githubusercontent.com/u/129792251?v=4", width=150)
-    col4.subheader("")
-    col4.subheader("🛠️  Crafted by VictorF313 (Sharique)")
+    st.subheader("🛠️  Crafted by VictorF313 (Sharique)")
